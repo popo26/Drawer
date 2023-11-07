864743503ba5d4e7059794c1d6d7b09527e7756f
popo26 <mynameispopo26@gmail.com>
2023-11-08 11:36:44 +1300

    drowdown listing functionality done

--- src/pages/SortPreviewPage.jsx
+++ src/pages/SortPreviewPage.jsx
@@ -19,10 +19,56 @@ export default function SortPreviewPage({ data }) {
   //         : data["scriblles"].map((scrb) => scrb.drawerId == item.id);
   //     });
 
+  const FindSubDrawers = () => {
+    const x = data["drawers"].filter(
+      (item) => item.id == state.selectedDrawerId
+    );
+    //console.log(x);
+    const renderedChildren =
+     x[0]['sub-drawer']===true
+        ? data["drawers"]
+            .filter((sub) => sub.drawerId == x[0].id)
+            .map((sub) => (
+              <p key={sub.id}>
+                ID:{sub.id}:{sub.name}
+              </p>
+            ))
+        : data["scribbles"]
+            .filter((scrb) => scrb.drawerId == x[0].id)
+            .map((scrb) => (
+              <p key={scrb.id}>
+                ID:{scrb.id}:{scrb.title}
+              </p>
+            ));
+
+    // const renderedChildren = () => {
+    //     console.log("X subdrawer", x[0]['sub-drawer'])
+    //   if (x[0]['sub-drawer'] === true) {
+    //     for (let y in data["drawers"]) {
+    //       if (data["drawers"][y].drawerId == x[0].id) {
+    //         console.log(data["drawers"][y]);
+    //       }
+    //     }
+    //   } else {
+    //     for (let z in data["scribbles"]) {
+    //       if (data["scribbles"][z].drawerId == x[0].id) {
+    //         console.log(data["scribbles"][z]);
+    //       }
+    //     }
+    //   }
+    // };
+    // renderedChildren()
+
+    return renderedChildren;
+  };
+
   return (
     <div>
       <p>Sort Preview - Selected Drawer ID: {state.selectedDrawerId}</p>
-      {renderedList}
+      <div>{renderedList}</div>
+      {/* <div>{findSubDrawers()}</div> */}
+      <FindSubDrawers />
+
       <div>
         <Icon
           icon="icon-park-outline:back"
