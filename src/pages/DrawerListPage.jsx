11158f3be2c352689be2bee5bc3a64cf8b1690b1
popo26 <mynameispopo26@gmail.com>
2023-11-12 08:45:29 +1300

    contentEditable in progress

--- src/pages/DrawerListPage.jsx
+++ src/pages/DrawerListPage.jsx
@@ -1,7 +1,9 @@
 import "../css/DrawerListPage.css";
 import { Icon } from "@iconify/react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
+import sanitizeHtml from "sanitize-html";
+import ContentEditable from "react-contenteditable";
 
 export default function DrawerListPage({
   data,
@@ -9,25 +11,52 @@ export default function DrawerListPage({
   selectedDrawerId,
   setSelectedDrawerId,
   drawerToBeMoved,
-  setDrawerToBeMoved
+  setDrawerToBeMoved,
 }) {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [drawerNameToEdit, setDrawerNameToEdit]=useState("");
-  const [isContentEditable, setIsContentEditable]=useState(false)
-
+  const [drawerNameToEdit, setDrawerNameToEdit] = useState("");
+  const [isContentEditable, setIsContentEditable] = useState(false);
 
   // console.log("Passed ID", id);
   // console.log("passed data", data);
   // console.log("passed expandedIndex", expandedIndex);
 
-  const handleEdit=(e)=>{
-    console.log("Edit clicked");
-    setIsContentEditable(!isContentEditable);
-    console.log(e.target.value)
-    setDrawerNameToEdit(e.target.value)
+  const handleSelectedDrawer = (clickedId) => {
+    const drawerName = data["drawers"].filter((item) => item.id == clickedId);
+    setDrawerNameToEdit(drawerName[0]["name"]);
+  };
 
-  }
+  console.log("Clicked ID", drawerNameToEdit)
+
+  const onContentChange = useCallback((evt) => {
+    const sanitizeConf = {
+      allowedTags: ["b", "i", "a", "p"],
+      allowedAttributes: { a: ["href"] },
+    };
+
+    setDrawerNameToEdit(
+      sanitizeHtml(evt.currentTarget.innerHTML, sanitizeConf)
+    );
+
+    //PUT request to add
+  }, []);
+
+  useEffect(() => {
+    const drawerName = data["drawers"].filter((item) => item.id == id);
+    setDrawerNameToEdit(drawerName[0]["name"]);
+
+    return () => {
+      console.log("cleanup");
+    };
+  }, []);
+
+  const handleEdit = (e) => {
+    console.log("Edit clicked");
+    // setIsContentEditable(!isContentEditable);
+    console.log(e.target.value);
+    // setDrawerNameToEdit(e.target.value);
+  };
 
   // ++++++++++++++ Find Scribbles +++++++++++++++++++++++++++++++++++++++++++++
   const findScribbles = (id, scribbles) => {
@@ -69,36 +98,53 @@ export default function DrawerListPage({
         const scribbleList = findScribbles(item.id, data["scribbles"]);
         return (
           <div key={item.id} className="sub-drawer-header">
-            <h3 className={"sub-drawer indent-" + item.level} >
-              {item.name}
+            {/* <h3 className={"sub-drawer indent-" + item.level}>
+              {item.name} */}
 
-              <Icon
-                onClick={() => alert("Are you sure to delete this sub-drawer?")}
-                icon="ion:trash-outline"
-                color="black"
-                width="20"
+            <h3
+              style={{ display: "inline-block" }}
+              className={"sub-drawer indent-" + item.level}
+              onClick={() => {
+                handleSelectedDrawer(item.id);
+              }}
+            >
+              <ContentEditable
+                onChange={onContentChange}
+                // onChange={handleChange2}
+                // html={drawerNameToEdit}
+                html={item.name}
+
+                // value={drawerNameToEdit}
               />
+            </h3>
 
-              {/* <Link
+            <Icon
+              onClick={() => alert("Are you sure to delete this sub-drawer?")}
+              icon="ion:trash-outline"
+              color="black"
+              width="20"
+            />
+
+            {/* <Link
                 to={`/sort-preview`}
                 onClick={() => setSelectedDrawerId(item.id)}
               > */}
-               <button 
-               onClick={() => {
+            <button
+              onClick={() => {
                 // selectedDrawerId = item.id
                 // setSelectedDrawerId(selectedDrawerId)
                 // let passingData = { selectedDrawerId };
-                drawerToBeMoved = item.id
-                setDrawerToBeMoved(drawerToBeMoved)
+                drawerToBeMoved = item.id;
+                setDrawerToBeMoved(drawerToBeMoved);
                 let passingData = { drawerToBeMoved };
                 console.log("PassingData", passingData);
                 navigate("/sort-drawer", { state: passingData });
               }}
             >
-                <Icon icon="mingcute:drawer-line" color="black" width="22" />
-                </button>
-              {/* </Link> */}
-            </h3>
+              <Icon icon="mingcute:drawer-line" color="black" width="22" />
+            </button>
+            {/* </Link> */}
+            {/* </h3> */}
             {scribbleList.length === 0 ? (
               <h6 className="no-scribble">No Scribbles</h6>
             ) : (
@@ -114,35 +160,68 @@ export default function DrawerListPage({
     }
   };
 
+  // const text = useRef(null);
+
+  // const handleChange2 = (evt) => {
+  //   text.current = evt.target.value;
+  // };
+
+  console.log("drawer name to edit", drawerNameToEdit);
+
   const renderedList = data["drawers"].map((item) => {
     if (id == item.id) {
       return (
         <div key={item.id}>
           <div>
-          <h2 contentEditable={isContentEditable} style={{display:"inline-block"}} value={drawerNameToEdit}>
-            ID:{item.id}, {item.name}
-            </h2>            
+            {/* <h2 contentEditable={isContentEditable} style={{display:"inline-block"}} value={drawerNameToEdit}>
+            {item.name}
+            </h2>             */}
+            <h2
+              // contentEditable={isContentEditable}
+              style={{ display: "inline-block" }}
+              // value={drawerNameToEdit}
+              // ref={text}
+              onClick={() => {
+                handleSelectedDrawer(item.id);
+              }}
+            >
+              <ContentEditable
+                onChange={onContentChange}
+                // onChange={handleChange2}
+                // html={drawerNameToEdit}
+                html={item.name}
+
+                // value={drawerNameToEdit}
+              />
+            </h2>
             <Icon
-              onClick={() => {alert("Are you sure to delete whole folder?")}}
+              onClick={() => {
+                alert("Are you sure to delete whole folder?");
+              }}
               icon="ion:trash-outline"
               color="black"
               width="20"
             />
-              <Icon 
-              icon="mingcute:drawer-line" 
-              color="black" 
-              width="22" 
+            <Icon
+              icon="mingcute:drawer-line"
+              color="black"
+              width="22"
               onClick={() => {
-                drawerToBeMoved = item.id
-                setDrawerToBeMoved(drawerToBeMoved)
+                drawerToBeMoved = item.id;
+                setDrawerToBeMoved(drawerToBeMoved);
                 let passingData = { selectedDrawerId, drawerToBeMoved };
                 console.log("PassingData", passingData);
                 navigate("/sort-drawer", { state: passingData });
               }}
-              />
-            <Icon icon="uiw:edit" color="black" width="22" onClick={handleEdit} />
-            </div>
-        
+            />
+            <Icon
+              icon="uiw:edit"
+              color="black"
+              width="22"
+              onClick={handleEdit}
+            />
+          </div>
+
           {item["sub-drawer"] === true ? (
             <div>
               <div className="no-subfolder">
